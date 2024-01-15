997a2049e8d34c071f63eabb517e44649f6c983b
Sasha <sasha.p@hush.com>
2024-01-15 11:19:10 +0300

    ddate added

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # go-go-go
+
+learning go for fun and profit
+
+## ddate
+Gregorian to POEE calendar as json converter
---
+++ ddate/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var dayNames = [...]string{"Sweetmorn", "Boomtime", "Pungenday", "Prickle-Prickle", "Setting Orange"}
+var seasonNames = [...]string{"Chaos", "Discord", "Confusion", "Bureaucracy", "The Aftermath"}
+var holydaysFive = [...]string{"Mungday", "Mojoday", "Syaday", "Zaraday", "Maladay"}
+var holydaysFifty = [...]string{"Chaoflux", "Discoflux", "Confuflux", "Bureflux", "Afflux"}
+
+const (
+	daysInYear         = 365
+	initialYearPadding = 1166
+	hailEris           = daysInYear / len(seasonNames)
+)
+
+type discordianDate struct {
+	Day     string `json:"day"`
+	Number  int    `json:"number"`
+	Season  string `json:"season"`
+	Year    int    `json:"year"`
+	Holyday string `json:"holyday"`
+}
+
+func getCurrentDate(w http.ResponseWriter, r *http.Request) {
+	now := time.Now()
+	result := convert(now)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
+}
+
+func convert(now time.Time) discordianDate {
+	ddate := discordianDate{}
+
+	discordianDay := (now.YearDay() - 1) / hailEris
+
+	ddate.Year = now.Year() + initialYearPadding
+	ddate.Season = seasonNames[discordianDay]
+	ddate.Day = dayNames[discordianDay+1]
+	ddate.Number = (now.YearDay()-1)%hailEris + 1
+
+	if ddate.Number == 5 {
+		ddate.Holyday = holydaysFive[now.YearDay()%5]
+	}
+
+	if ddate.Number == 50 {
+		ddate.Holyday = holydaysFifty[now.YearDay()%5]
+	}
+
+	if int(now.Month()) == 2 && now.Day() == 29 {
+		ddate.Holyday = "St. Tib's Day"
+	}
+
+	return ddate
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", getCurrentDate).Methods("GET")
+
+	port := 8080
+	fmt.Printf("Server is running on :%d\n", port)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), router)
+}
